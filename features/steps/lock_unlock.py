17d1d7dd74ea5c8282ef815d58f528e0a4ef9aa5
do Nascimento de Sousa, Robson (R.L.) <robsonluan273@hotmail.com>
2025-07-22 15:26:37 -0300

    fixed background steps

--- features/steps/lock_unlock.py
+++ features/steps/lock_unlock.py
@@ -2,6 +2,11 @@ from behave import *
 from features.environment import ModelManager
 import time
 
+def get_all_doors_state(context) -> list[int]:
+    """
+    Returns the state of all doors as a list.
+    """
+    return [int(context.model.read_from_model(f'door_lock_state_{i}')) for i in range(1, 5)]
 
 @given('all doors are {state}')
 def step_given_all_doors_are_in_state(context:any, state:str):
@@ -16,6 +21,12 @@ def step_given_all_doors_are_in_state(context:any, state:str):
     else:
         raise ValueError('state must be either locked or unlocked')
 
+    context.model.write_to_model('manual_lock_rqst', 0)
+    time.sleep(0.2)
+    context.model.write_to_model('manual_lock_rqst', 1)
+    time.sleep(0.2)
+    context.model.write_to_model('manual_lock_rqst', 0)
+
     step_then_all_doors_should_be(context, state)
 
 @given("my vehicle is {state} with no release buttons pressed")
@@ -25,34 +36,35 @@ def step_given_my_vehicle_is_in_state(context:any, state:str):
     for i in range(4):
         context.model.write_to_model(f'door_release_{i+1}', 0)
 
-@given('the door {door_id} is {door_state}')
-def step_given_the_door_is_in_state(context:any, door_id:str, door_state:str):
+@given('the door {door_id} is {expected_state}')
+def step_given_the_door_is_in_state(context:any, door_id:str, expected_state:str):
     door_id = int(door_id.replace("'", "").replace('"', ''))
-    door_state = door_state.replace("'", "").replace('"', '')
+    expected_state = expected_state.replace("'", "").replace('"', '')
 
     if door_id < 1 or door_id > 4:
         raise ValueError('door_id must be between 1 and 4')
 
-    current_state = context.model.read_from_model('current_door_state')
-    assert current_state >= 0 and current_state < 16, 'Failed to get the current_door_state'
+    current_state = get_all_doors_state(context)
 
-    if door_state == 'locked':
-        current_state |= 2**(door_id-1)
+    if expected_state == 'locked':
+        current_state[door_id] = 1
 
-    elif door_state == 'unlocked':
-        current_state &= ~(2**(door_id-1))
+    elif expected_state == 'unlocked':
+        current_state[door_id] = 0
 
     else:
         raise ValueError('door_state must be either locked or unlocked')
 
-    context.model.write_to_model(f'general_locking_manager/set_state_value', current_state, 'main')
-    context.model.write_to_model(f'general_locking_manager/trigger_state_set', 0, 'main')
-    time.sleep(0.2)
-    context.model.write_to_model(f'general_locking_manager/trigger_state_set', 1, 'main')
-    time.sleep(0.2)
-    context.model.write_to_model(f'general_locking_manager/trigger_state_set', 0, 'main')
+    set_state = sum(2**i for i, state in enumerate(current_state) if state == 1)
+    context.model.write_to_model(f'manual_lock', set_state)
 
-    assert int(context.model.read_from_model('current_door_state')) == current_state, f'Failed to set the door {door_id} to {door_state}'
+    context.write_to_model('manual_lock_rqst', 0)
+    time.sleep(0.2)
+    context.write_to_model('manual_lock_rqst', 1)
+    time.sleep(0.2)
+    context.write_to_model('manual_lock_rqst', 0)
+
+    assert get_all_doors_state(context)[door_id] == current_state[door_id], f'Failed to set the door {door_id} to {expected_state}. Expected {current_state[door_id]}, got {get_all_doors_state(context)[door_id]}'
 
 @given('I {key_present} an authenticated key with me')
 def step_given_i_have_an_authenticated_key_with_me(context:any, key_present:str):
