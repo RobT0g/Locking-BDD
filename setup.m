68e37b6dfecb92ebd41ca918997c6784460e2280
do Nascimento de Sousa, Robson (R.L.) <140737277+RDONASC1_ford@users.noreply.github.com>
2025-08-03 12:52:32 -0300

    Updated setup

--- setup.m
+++ setup.m
@@ -1,15 +1,8 @@
 currentDir = pwd;
-filename = "feature_model.slx";
-
 cd(currentDir);
 addpath(genpath(currentDir));
 
-model = strcat(strrep(filename, '.slx', ''),"/");
-open_system(model);
+model = "feature_model";
+open_system(strcat(model, "/"));
 
-try
-    matlab.engine.shareEngine;
-    disp('MATLAB engine shared successfully.');
-catch ME
-    disp(['Error sharing MATLAB engine: ', ME.message]);
-end
+matlab.engine.shareEngine;
